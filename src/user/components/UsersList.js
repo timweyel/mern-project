91636e6d9a91c97d02ade86f6242aae23d0ac288
TimWeyel <tweyel@gmail.com>
2021-09-28 13:54:03 -0700

    added UserItem component

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -5,24 +5,25 @@ import './UsersList.css'
 
 const UsersList = props => {
   if (props.items.length === 0) {
-    return (
-      <div className="center">
-        <h2>No users found.</h2>
+    return ( 
+      <div className = "center" >
+        <h2> No users found. </h2> 
       </div> 
     );
   }
 
-  return <ul>
-    {props.items.map(user => (
-      <UserItem 
-        key={user.id} 
-        id={user.id} 
-        image={user.image} 
-        name={user.name} 
-        placeCount={user.places}
-      />
-    ))}
-  </ul>
+  return (
+    <ul className="users-list"> 
+      {props.items.map(user => ( 
+        <UserItem 
+          key = {user.id}
+          id = {user.id}
+          image = {user.image}
+          name = {user.name}
+          placeCount = {user.places}
+        />
+      ))} 
+    </ul>)
 };
 
 export default UsersList;
