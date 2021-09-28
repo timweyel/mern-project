c64e14298e46580d0f696d1964c7ce7072a5e721
TimWeyel <tweyel@gmail.com>
2021-09-28 13:38:51 -0700

    added UserList component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Halsey',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Halsey_2019_by_Glenn_Francis_%28cropped%29_2.jpg/220px-Halsey_2019_by_Glenn_Francis_%28cropped%29_2.jpg',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
