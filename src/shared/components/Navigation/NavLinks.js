58ca626d5bb95b4c2e9c1d2bc555715c8a2dc1fa
TimWeyel <tweyel@gmail.com>
2021-09-29 16:14:38 -0700

    added side drawer for mobile view

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import { NavLink } from 'react-router-dom'
-;
 import './NavLinks.css';
 
 const NavLinks = props => {
   return <ul className="nav-links">
     <li>
-      <NavLink to="/ exact">ALL USERS</NavLink>
+      <NavLink to="/" exact>ALL USERS</NavLink>
     </li>
     <li>
       <NavLink to="/u1/places">MY PLACES</NavLink>
