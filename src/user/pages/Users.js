7f49f23fa5c637ec3d725534f1e0444e0ebff86a
TimWeyel <tweyel@gmail.com>
2021-10-12 14:06:36 -0700

    added animation to sideDrawer

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -7,7 +7,8 @@ const Users = () => {
     {
       id: 'u1',
       name: 'Halsey',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Halsey_2019_by_Glenn_Francis_%28cropped%29_2.jpg/220px-Halsey_2019_by_Glenn_Francis_%28cropped%29_2.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Halsey_2019_by_Glenn_Francis_%28cropped%29_2.jpg/1200px-Halsey_2019_by_Glenn_Francis_%28cropped%29_2.jpg',
       places: 3
     }
   ];
